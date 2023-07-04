49098f191731aac3fe3dd8ff36ec953598e872e4
AbineshSivakumar <abineshsivakumar@gmail.com>
2023-07-04 19:29:06 +0000

    version_1

---
+++ anaconda-setup.sh
@@ -0,0 +1,28 @@
+[12:51 am] Pranav Unnikrishnan - [CB.EN.U4AIE20053]
+
+#!/bin/bash
+
+
+# Download and install Miniconda (a lightweight version of Anaconda)
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+
+bash ~/miniconda.sh -b -p ~/miniconda
+
+rm ~/miniconda.sh
+
+
+# Add conda to the system PATH
+
+echo 'export PATH="$HOME/miniconda/bin:$PATH"' >> ~/.bashrc
+
+source ~/.bashrc
+
+
+# Activate the conda base environment
+
+conda init
+
+conda create -n qlora -y
+
+conda activate qlora
